89e02c8de0f03ee25c2dc7162c7a4b6da581cc71
Otto <otto.sys.lab@gmail.com>
2021-01-09 11:19:21 +0800

    第二次重構read basic database

--- source/GtcsDatabase.cpp
+++ source/GtcsDatabase.cpp
@@ -21,7 +21,6 @@ Sqlite3Manager::Sqlite3Manager()
 // Disturctor.
 Sqlite3Manager::~Sqlite3Manager()
 {}
-
 // Write data to sqlite.
 bool Sqlite3Manager::UpdateDatabase(std::string db_Path,std::string table,std::string sqlcmd)
 {
@@ -62,11 +61,12 @@ bool Sqlite3Manager::UpdateDatabase(std::string db_Path,std::string table,std::s
 // Write data to sqlite.
 bool Sqlite3Manager::ReadDatabase(std::string db_Path, std::string table,std::string *ptr)
 {
+    // Initial sql cmd.
+    std::string sqlcmd = "SELECT * from "+ table ;
     // Initial value.
     sqlite3 *db;
     sqlite3_stmt *stmt;
     int rc;
-    std::string sql  = "";
     // Open database.
     rc = sqlite3_open(db_Path.c_str(),&db);
     if (rc)
@@ -75,12 +75,7 @@ bool Sqlite3Manager::ReadDatabase(std::string db_Path, std::string table,std::st
         return false;
     }
     // Send SQL statement to db.
-    sql = "SELECT * from "+ table;
-    rc = sqlite3_prepare_v2(db,
-                            sql.c_str(),
-                            -1,
-                            &stmt,
-                            NULL);
+    rc = sqlite3_prepare_v2(db,sqlcmd.c_str(),-1,&stmt,NULL);
     if (rc != SQLITE_OK)
     {
         std::cout<<"Read SQL error:"<<sqlite3_errmsg(db)<<std::endl;
@@ -150,6 +145,7 @@ void GtcsDatabase::SetDatabasePath(std::string Path)
 {
     dbPath = Path;
 }
+// Basic table.
 bool GtcsDatabase::UpdateDatabaseBasicTable(GtcsDatabaseBasicInfo &db_basic)
 {
     // Initial sql command.
@@ -206,6 +202,75 @@ bool GtcsDatabase::UpdateDatabaseBasicTable(GtcsDatabaseBasicInfo &db_basic)
 }
 bool GtcsDatabase::ReadDatabaseBasicTable(GtcsDatabaseBasicInfo &db_basic)
 {
+    // Initial sqlcmd.
+    std::string sqlcmd = "SELECT * from basic where rowid = 1;";
+    // Initial value.
+    sqlite3 *db;
+    sqlite3_stmt *stmt;
+    int rc;
+    // Open database.
+    rc = sqlite3_open(dbPath.c_str(),&db);
+    if (rc)
+    {
+        std::cout<<"Can't open database : "<< sqlite3_errmsg(db) <<std::endl;
+        return false;
+    }
+    // Send SQL statement to db.
+    rc = sqlite3_prepare_v2(db,sqlcmd.c_str(),-1,&stmt,NULL);
+    if (rc != SQLITE_OK)
+    {
+        std::cout<<"Read SQL error:"<<sqlite3_errmsg(db)<<std::endl;
+        sqlite3_finalize(stmt);
+        return false;
+    }
+    // Excute step.
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW && rc != SQLITE_DONE)
+    {
+        // string errmsg(sqlite3_errmsg(db));
+        std::cout<<" Read SQL error:"<<sqlite3_errmsg(db)<<std::endl;
+        sqlite3_finalize(stmt);
+        return false;
+    }
+    if (rc == SQLITE_DONE) {
+        sqlite3_finalize(stmt);
+        std::cout<<"customer not found"<<std::endl;
+        return false;
+    }
+
+    // Get data from sqlite_clomn_text pointer. 
+    int columnnames_size  = db_basic.columnnames.size();
+    for (int i = 0; i < columnnames_size; i++)
+    {
+        if (db_basic.type[db_basic.columnnames[i]]!="TEXT")
+        {
+            sqlcmd += db_basic.columnnames[i] + " = " + db_basic.data[db_basic.columnnames[i]] + ",";   
+        }   
+        else
+        {
+            sqlcmd += db_basic.columnnames[i] + " = " + "'" + db_basic.data[db_basic.columnnames[i]] + "'" +",";   
+        }
+    }
+    sqlcmd = sqlcmd.replace(sqlcmd.end()-1,sqlcmd.end()," ");
+    sqlcmd += "where rowid = 1;";
+
+
+    // Assign dat to bsic struct.
+    int columnname_size  = db_basic.columnnames.size();    
+    for (int i = 0; i < columnname_size; i++)
+    {
+        if (sqlite3_column_text(stmt, i)==NULL) // If get data == null,break the while loop.
+        {
+            return false;
+        } 
+        else
+        {
+            db_basic.data[db_basic.columnnames[i]] = std::string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, i)));
+        }
+    }
+    // Close sqlite3.
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
     return true;
 }
 #pragma endregion
